6c3dd1e6abec26665968f933ede14fd218eec686
Charles H <chathaway@logrit.com>
2013-01-31 13:42:07 -0500

    Initial commit

---
+++ login_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LoginApp::Application.config.secret_token = '852e91b940b428acbad6948e82beb9ec7f569a24e87575003126819114425731f1cecf7d1c348596dc84542fa7e26e74dd83864d4993730a54ee0a70ae2f96cc'
